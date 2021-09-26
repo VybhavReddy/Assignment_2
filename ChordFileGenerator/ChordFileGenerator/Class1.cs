f11b4021beb9e1a10c1df3080dcdeb40dcd2d94e
Vybhav A <a.vybhav.reddy@gmail.com>
2021-09-26 20:24:41 +0530

    assignment files added

---
+++ ChordFileGenerator/ChordFileGenerator/Class1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ChordFileGenerator
+{
+    public enum FileType
+    {
+        Chords,
+        Tab,
+        Lyrics
+    }
+    public class Class1
+    {
+        public string SongName { get; set; }
+        public string Artist { get; set; }
+        public FileType FileType { get; set; }
+        private List<string> lines;
+        FileType filetype;
+
+
+        public void getdata(object b,params string[] s)
+        {
+
+            SongName = s[0];
+            Artist = s[1];
+            for (; ; )
+            {
+                Console.WriteLine("Enter the input type\n'0' if you are entering chord\n'1' if you are entering a tab\n'2' if you are entering the lyrics\nEnter any other number to end");
+                int ch = Console.Read();
+
+                if (ch == 0)
+                    FileType = FileType.Chords;
+                else if (ch == 1)
+                    FileType = FileType.Tab;
+                else if (ch == 2)
+                    FileType = FileType.Lyrics;
+                else
+                {
+                    Console.WriteLine("Wrong selection");
+                    break;
+                }
+                filetype = FileType;
+
+                switch (filetype)
+                {
+                    case FileType.Chords:
+                        {
+                            Console.WriteLine("Enter the chord line");
+                            string str = Console.ReadLine();
+                            lines.Add("Chord: " + str);
+                            break;
+                        }
+                    case FileType.Tab:
+                        {
+                            Console.WriteLine("You have selected tab");
+                            string str = Console.ReadLine();
+                            lines.Add("     Tab      ");
+                            break;
+                        }
+                    case FileType.Lyrics:
+                        {
+                            Console.WriteLine("Enter the lyric line");
+                            string str = Console.ReadLine();
+                            lines.Add("Lyric: " + str);
+                            break;
+                        }
+                }
+            }
+            FileStream fs = new FileStream(@"C:\c# training\ASSIGNMENT_2\test.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine("Songname: " + SongName);
+            sw.WriteLine("Artist: " + Artist);
+            foreach (string str in lines)
+            {
+                sw.WriteLine(str);
+            }
+        }
+        
+    }
+}
---
+++ assignment_2/assignment_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly A_name = Assembly.LoadFile(@"C:\c# training\ASSIGNMENT_2\ChordFileGenerator\ChordFileGenerator\bin\Debug\ChordFileGenerator.dll");
+            Type name = A_name.GetType("ChordFileGenerator.Class1");
+            object obj = Activator.CreateInstance(name);
+            Console.WriteLine("Enter the song name");
+            String[] S = new String[2];
+            S[0]= Console.ReadLine();
+            Console.WriteLine("Enetr the name of artist");
+            S[1] = Console.ReadLine();
+            MethodInfo getdata = name.GetMethod("getdata");
+            PropertyInfo p = obj.GetType().GetProperty("SongName");
+            p.SetValue(obj,S[0]);
+             p = obj.GetType().GetProperty("Artist");
+            p.SetValue(obj, S[1]);
+            getdata.Invoke(obj, S);
+            Console.Read();
+        }
+    }
+}
